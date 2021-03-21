d9659488f5df32fc295bc0c832b5c3b96a9a15f8
Joni Mettälä <joni.mettala@gmail.com>
2021-03-21 11:54:13 +0200

    Improve note showing
    If there are no notes, tell it instead of showing nothing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Grid from '@material-ui/core/Grid'
+
 import './App.css'
 import NavBar from './NavBar'
-
 import noteService from './services/notes'
 import NoteList from './NoteList'
 
--- client/src/NoteList.js
+++ client/src/NoteList.js
@@ -3,9 +3,11 @@ import React from 'react'
 import NoteCard from './NoteCard'
 
 const NoteList = ({ notes }) => {
-  console.log(notes)
-
-  return notes.map((note) => <NoteCard key={note.id} note={note} />)
+  if (notes.length > 0) {
+    return notes.map((note) => <NoteCard key={note.id} note={note} />)
+  } else {
+    return <p>No notes to show.</p>
+  }
 }
 
 export default NoteList
